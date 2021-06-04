89819a645a68c31ce5b7375f80591adf08bdaba1
kazuki <pr76.engineer@gmail.com>
2021-06-04 19:29:41 +0900

    mainmenu&newpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get get"/" => "posts#index"
+  get "/" => "posts#index"
+  get "/show" => "posts#show"
+  get "/new" => "posts#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
